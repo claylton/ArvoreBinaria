5462225be6737dbc5b5aff6972fffd7df451d3a8
claylton <clayltonsp@hotmail.com>
2020-09-09 18:24:25 -0300

    Tirando construtor nó

--- src/arvorebinaria/No.java
+++ src/arvorebinaria/No.java
@@ -16,12 +16,12 @@ public class No<T> {
     private No<T> esquerda;
     private No<T> direita;
 
-    public No(T valor, No<T> pai, No<T> esquerda, No<T> direita) {
+/*    public No(T valor, No<T> pai, No<T> esquerda, No<T> direita) {
         this.valor = valor;
         this.pai = pai;
         this.esquerda = null;
         this.direita = null;
-    }
+    }*/
 
     
     public T getValor() {
